404be6026a10b32bf80959b8eb9035da5c4efb55
JrAdilson <imjrgonc@gmail.com>
2022-04-24 20:33:58 -0300

    testing Chalk dependences

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Lets start!')
+import chalk from 'chalk';
+
+//encadear métodos para colorir texto, cor de fundo e texto em negrito
+console.log(chalk.blue.bgWhite.bold('Alura'));
+
+//receber múltiplos argumentos
+console.log(chalk.blue('curso', 'de', 'NodeJS'));
+
+//métodos aninhados
+console.log(chalk.red('vermelho', chalk.underline.bgBlue('azul')));
+
+// uso de template strings e placeholders
+console.log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
+// Chalk = Encontrar uma palavra específica dentro do console, por exemplo.
