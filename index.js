022ac8827a859ccb5d35b36ba7b3085a8810c796
JrAdilson <imjrgonc@gmail.com>
2022-04-24 20:52:36 -0300

    using chalk and fs libs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import chalk from 'chalk';
-
+import fs from 'fs';
 //encadear métodos para colorir texto, cor de fundo e texto em negrito
 console.log(chalk.blue.bgWhite.bold('Alura'));
 
@@ -16,3 +16,17 @@ RAM: ${chalk.green('40%')}
 DISK: ${chalk.yellow('70%')}
 `);
 // Chalk = Encontrar uma palavra específica dentro do console, por exemplo.
+function getError(err){
+    throw new Error(chalk.red(err.code, 'Dont have any archive on this way'))
+}
+function getArchive(wayArchive){
+    //fs.readFile(wayArchive, encoding = 'utf-8')
+    const encoding = 'utf-8'
+    fs.readFile(wayArchive, encoding, (err, text) => {
+        if(err){
+            getError(err);
+        }
+        console.log(chalk.black(text))
+    })
+}
+getArchive('./')
